3ceee7f4a7e1f377ee0babb2ba68dfc7c30972b3
nanqing.zhu <nanqing.zhu@unicle.life>
2022-03-17 22:08:25 -0400

    add sample to retrieve intersection geometry from database

--- sample_bgc_hexgrid_leaflet.R
+++ sample_bgc_hexgrid_leaflet.R
@@ -54,12 +54,58 @@ pool <- dbPool(
   password = Sys.getenv("BCGOV_PWD")
 )
 
+
+#retrieve grid id by bound box coordinates:
+lon <- c(-131.28, -131.5)
+lat <- c(52.40, 52.60)
+
+poly_df <- data.frame(lon, lat)
+
+poly <- st_as_sf(poly_df, coords = c("lon", "lat"), crs = 4326)%>%
+        st_transform(3005)%>%
+        st_bbox() %>% 
+        st_as_sfc()%>%
+        st_as_text()
+
+poly <- "POLYGON ((949328.2 1039597, 985443.5 1039597, 985443.5 1070499, 949328.2 1070499, 949328.2 1039597))"
+#look up hex_grid table from boundbox:
+dat <- st_read(pool,
+                          query = paste0("select siteno, district, geom
+                                   from grid_dist 
+                                   where st_intersects(geom, 'SRID=3005;",poly,"');"))
+
+bcg_predictions <- dbGetCCISSRaw(pool,dat$siteno,"ACCESS-ESM1-5", "ssp126","2001")
+
+
+
+
+dat <- dat %>%
+  left_join(bcg_predictions[, c('siteno', 'bgc_pred')])%>%
+  left_join(bgc_colors, by = c('bgc_pred' = 'BGC'))%>%
+  st_transform(crs = 4326)
+
+
+dat %>%
+  leaflet() %>%
+  #setView(lng = -131.28, lat = 52.40, zoom = 11) %>%
+  addProviderTiles("CartoDB.Positron")%>%
+  addPolygons(
+    fillColor = ~ Col,
+    fillOpacity = 0.7,
+    color = ~ Col
+  )
+
+
+
 #plot hexgrid by district: Haida Gwaii Natural Resource District
+#check projection
+dbGetQuery(pool, "SELECT ST_SRID(geom) from grid_dist limit 1")
 
 hex_sf <- st_read(pool,
                   query = "select a.siteno, a.district, a.geom
                          from grid_dist a
-                         where a.district = 'Haida Gwaii Natural Resource District' ")
+                         where a.district = 'Haida Gwaii Natural Resource District' 
+                         limit 500")
 
 #join prediction results
 gcmOpts <- dbGetQuery(pool, "select gcm from gcm")[1,1]
@@ -74,14 +120,14 @@ bcg_predictions <- dbGetCCISSRaw(pool,hex_sf$siteno,gcmOpts,scenarioOpts,periodO
 hex_map <- hex_sf %>%
            left_join(bcg_predictions[, c('siteno', 'bgc_pred')])
 
-hex_map <- aggregate(hex_map, by = list(hex_map$bgc_pred), FUN = mean)
+#hex_map <- aggregate(hex_map, by = list(hex_map$bgc_pred), FUN = mean)
 
 # join prediction color schema
 colors <- read.csv('WNA_v12_HexCols.csv')
 
 hex_map <- hex_map %>%
-           left_join(colors, by = c('Group.1' = 'BGC'))
-           #left_join(colors, by = c('bgc_pred' = 'BGC'))
+           #left_join(colors, by = c('Group.1' = 'BGC'))
+           left_join(colors, by = c('bgc_pred' = 'BGC'))
 
 
 #apply correct projection
@@ -96,7 +142,7 @@ system.time(
 #overlap hex grid on leaflet
 hex %>%
 leaflet() %>%
-  setView(lng = -126.5, lat = 54.5, zoom = 5) %>%
+  setView(lng = -131.28, lat = 52.40, zoom = 12) %>%
   addProviderTiles("CartoDB.Positron")%>%
   addPolygons(
     fillColor = ~ Col,
